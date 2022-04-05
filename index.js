21fdcd52e3a6371be3c13c2f315b5e2c1b8dbc00
Craig Leslie <cleslie25@gmail.com>
2022-04-04 22:07:59 -0500

    Initial commit.

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const inquirer = require('inquirer');
+const mysql2 = require('mysql2');
+const table = require('console.table');
+
+const Department = require ('./lib/Department');
+const Role = require ('./lib/Role');
+const Employee = require ('./lib/Employee');
+
